3facb20189baa097c06ad4a405425de26fefac31
Brenden Konnagan <brendenk1@icloud.com>
2023-03-30 16:43:09 -0400

    Added AsyncPublisher

--- Sources/CavachonKit/protocols/AsyncMonitor.swift
+++ Sources/CavachonKit/protocols/AsyncMonitor.swift
@@ -1,7 +1,10 @@
+import Combine
 import Foundation
 
 /// Objects that conform to the `AsyncMonitor` interface will allow for the interaction of `Event` types using Swift Concurrency APIs.
 public protocol AsyncMonitor: Monitor {
+    /// A `Publisher` of `Event` types as received
+    func asyncPublisher() -> AnyPublisher<Event, Never>
     /// A `AsyncStream` of `Event` types as received
     func asyncStream() -> AsyncStream<Event>
     /// A function that allows for the monitoring of a particular event, returning once received.
@@ -9,6 +12,14 @@ public protocol AsyncMonitor: Monitor {
 }
 
 public extension AsyncMonitor {
+    func asyncPublisher() -> AnyPublisher<Event, Never> {
+        let publisher = PassthroughSubject<Event, Never>()
+        
+        self.handle = { publisher.send($0) }
+        
+        return publisher.eraseToAnyPublisher()
+    }
+    
     func asyncStream() -> AsyncStream<Event> {
         AsyncStream(Event.self) { continuation in
             self.handle = { continuation.yield($0) }
