e4d8fdf1fc33817bd28be36c7c2fb5de733fa64d
Brenden Konnagan <brendenk1@icloud.com>
2023-05-03 11:56:17 -0400

    Add AsyncTryMap

--- Sources/CavachonKit/Extensions/Sequence+.swift
+++ Sources/CavachonKit/Extensions/Sequence+.swift
@@ -1,6 +1,17 @@
 import Foundation
 
 public extension Sequence {
+    /// Performs a try map on the collection, allowing for a asynchronous handler on each item
+    func asyncTryMap<Result>(
+        transform: (Element) async throws -> Result
+    ) async throws -> [Result] {
+        var mapped: [Result] = []
+        for element in self {
+            mapped.append(try await transform(element))
+        }
+        return mapped
+    }
+    
     /// Performs a `reduce` on the collection, allowing for a asynchronous handler on each item
     func asyncReduce<Result>(
         _ initialResult: Result,
